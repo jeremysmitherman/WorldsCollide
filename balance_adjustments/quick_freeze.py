be81250835feb82a8d1c0e99f3cd2d7d69e8cf1f
Jers <jeremysmitherman@gmail.com>
2024-12-08 16:28:57 -0600

    bumped up to 16 which should usually be right around 30 seconds

--- balance_adjustments/quick_freeze.py
+++ balance_adjustments/quick_freeze.py
@@ -7,5 +7,5 @@ class QuickFreeze:
             self.quick_freeze_mod()
 
     def quick_freeze_mod(self):
-        Reserve(0x24695, 0x24695, "Freeze time", 0x0F)
+        Reserve(0x24695, 0x24695, "Freeze time", 0x11)
 
